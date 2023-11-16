068543ce0e96be8dbecd095770512baa5d714142
joykara <joykarani1@gmail.com>
2023-11-16 15:19:04 +0300

    start server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,11 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express()
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.listen(3002, () => console.log("Server Started!"))
