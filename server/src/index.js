e6f8120ca035d5577b17dea7e391c1da5504d880
joykara <joykarani1@gmail.com>
2023-11-21 12:20:31 +0300

    update model and user route endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-require("dotenv").config()
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { userRouter } from './routes/usersRoute';
 
 const app = express()
 
@@ -9,7 +12,10 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
+// Routes
+app.use('/auth', userRouter);
+
+mongoose.connect(process.env.MONGODB_URI)
 .then(
     () => {
         console.log("DB Connected!")
