974f101bc7a5cc344638b30e494dc911d74bf32d
joykara <joykarani1@gmail.com>
2023-12-24 18:35:48 +0300

    complete registration route

--- server/src/routes/usersRoute.js
+++ server/src/routes/usersRoute.js
@@ -7,11 +7,24 @@ const router = express.Router()
 // Register
 router.post('/register', async (req, res) => {
     const { username, password } = req.body;
-    // const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await UserModel.findOne({ username });
 
-    const user = await UserModel.findOne({ username })
+    if (user) {
+        return res.json("User Already Exists!");
+    }
+    // hash password for security
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.json(user);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+
+    // create token
+    // const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET);
+    // res.cookie("token", token, {
+    //     httpOnly: true,
+    //     maxAge: 24 * 60 * 60 * 1000 // 1 day
+    // });
+    res.json({ message: "User Registered Successfully!" });
 })
 
 // Login
