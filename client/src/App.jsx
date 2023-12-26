e3fa03a171c807ce64d13cec357771ac98a6521c
joykara <joykarani1@gmail.com>
2023-12-26 19:06:55 +0300

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,30 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+
 
   return (
     <>
-      <div className='m-4 '>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='underline bg-slate-600'>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>{counter}</p>
+        <button onClick={() => setCounter(counter + 1)}>
+          Increment
         </button>
-        <p className='underline'>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCounter(counter - 1)}>Decrement</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
