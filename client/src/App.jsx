539b47ed656272c90071d95a350ba72c723c2f80
joykara <joykarani1@gmail.com>
2024-01-01 17:03:19 +0300

    add page router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, CreateRecipe, Auth, Recipes } from './pages'
 import './App.css'
 
 function App() {
@@ -9,11 +9,15 @@ function App() {
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/recipe" exact element={<Recipes />} />
+        </Routes>
+      </Router>
       <div className="card">
         <p>{counter}</p>
-        <button onClick={() => setCounter(counter + 1)}>
-          Increment
-        </button>
+        <button onClick={() => setCounter(counter + 1)}>Increment</button>
         <button onClick={() => setCounter(counter - 1)}>Decrement</button>
       </div>
     </>
